2f13b49d88080752576c51ecf215da8af32ed136
ElderSousa <elder2faces@gmail.com>
2025-03-21 17:32:52 -0300

    Configuração de dependências

---
+++ MedSync.Domain/Entities/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿namespace MedSync.Domain.Entities;
+
+public class Pessoa
+{
+    public Guid Id { get; set; }
+    public string? Nome { get; set; }
+    public string? CPF { get; set; }
+    public string? RG { get; set; }
+    public string? Email { get; set; }
+    public char Sexo { get; set; }
+    public DateOnly? DataNascimento { get; set; }
+    public DateTime? CriadoEm { get; set; }
+    public DateTime? ModificadoEm { get; set; }
+    public Guid? CriadoPor { get; set; }
+    public Guid? ModificadoPor { get; set; }
+}
